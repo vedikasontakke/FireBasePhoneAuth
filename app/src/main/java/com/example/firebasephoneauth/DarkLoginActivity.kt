e9e174ad6bd3b2e6d29fdf9a6f46705943754840
Lenovo <vedikasontakke2003@gmail.com>
2021-04-10 19:33:09 -0300

    Initail commit

---
+++ app/src/main/java/com/example/firebasephoneauth/DarkLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebasephoneauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DarkLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dark_login)
+    }
+}
